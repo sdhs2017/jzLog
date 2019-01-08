a67e061577da8adb471ef04dbbf5f8591cbd8234
jiyourui <jiyourui@jyr-PC>
2019-01-08 14:47:44 +0800

    master-增加dns采集功能

--- jzLog/src/main/java/com/jz/bigdata/business/logAnalysis/log/LogType.java
+++ jzLog/src/main/java/com/jz/bigdata/business/logAnalysis/log/LogType.java
@@ -12,6 +12,7 @@ public class LogType {
 	public static final String LOGTYPE_APPLOG = "applog";
 	public static final String LOGTYPE_PACKETFILTERINGFIREWALL_LOG = "packetfilteringfirewall_log";
 	public static final String LOGTYPE_NETFLOW = "netflow";
+	public static final String LOGTYPE_DNS = "dns";
 
 	public static final String LOGTYPE_UNKNOWN = "unknown";	
 
