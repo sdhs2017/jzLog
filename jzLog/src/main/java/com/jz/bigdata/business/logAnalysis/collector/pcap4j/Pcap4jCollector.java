095bfce5b11abc8482088a14677ad0f3f212d8ab
jiyourui <jiyourui@jyr-PC>
2019-04-12 11:29:21 +0800

    master-pcap4j增加

---
+++ jzLog/src/main/java/com/jz/bigdata/business/logAnalysis/collector/pcap4j/Pcap4jCollector.java
@@ -0,0 +1,55 @@
+package com.jz.bigdata.business.logAnalysis.collector.pcap4j;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.pcap4j.core.PacketListener;
+import org.pcap4j.core.PcapAddress;
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapIpV4Address;
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.Pcaps;
+import org.pcap4j.core.BpfProgram.BpfCompileMode;
+import org.pcap4j.core.NotOpenException;
+import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
+import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.packet.Packet;
+import org.pcap4j.packet.TcpPacket;
+
+
+public class Pcap4jCollector implements Callable<String>{
+	
+	/**
+	 * 需要获取网卡信息的IP地址
+	 */
+	private String ip;
+	
+	static String client ="";
+	
+	private PcapHandle handle = null;
+	
+	private PacketListener listener =null;
+	
+	static HashMap<String, TcpStream> tcpStreamList=new HashMap<String, TcpStream>();
+	
+	public Pcap4jCollector(String ip,PcapHandle handle,PacketListener listener) {
+		this.ip = ip;
+		this.handle = handle;
+		this.listener = listener;
+	}
+
+	@Override
+	public String call() throws InterruptedException, PcapNativeException, NotOpenException {
+    	
+        //直接使用loop
+        handle.loop(1000000, listener);
+	
+		return null;
+	}
+	
+	
+
+
+}
