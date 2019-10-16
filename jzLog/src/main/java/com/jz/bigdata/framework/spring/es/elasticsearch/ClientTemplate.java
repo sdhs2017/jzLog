7e579f20c5652b32fbd3464751095c753f1853f1
jiyourui <rui.n@foxmail.com>
2019-10-16 16:11:02 +0800

    master-回退到5.4.0版本

--- jzLog/src/main/java/com/jz/bigdata/framework/spring/es/elasticsearch/ClientTemplate.java
+++ jzLog/src/main/java/com/jz/bigdata/framework/spring/es/elasticsearch/ClientTemplate.java
@@ -26,9 +26,9 @@ import org.elasticsearch.action.admin.indices.stats.IndicesStatsResponse;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 // 5.4.0版本
-//import org.elasticsearch.action.bulk.byscroll.BulkByScrollResponse;
+import org.elasticsearch.action.bulk.byscroll.BulkByScrollResponse;
 // 5.6.3版本
-import org.elasticsearch.index.reindex.BulkByScrollResponse;
+//import org.elasticsearch.index.reindex.BulkByScrollResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
